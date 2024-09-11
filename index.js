5e398c11360569a6bbd66f52558bbacf38652950
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-09-11 01:25:34 +0100

    broke down the lengthy function into modular functions, made the code cleaner and less recursive. editted in an online edittor first

--- index.js
+++ index.js
@@ -1,74 +1,51 @@
-let initialPrompt = prompt("Would you like to play Rock, Paper, Scissors? ");
+// add function that ends the game if the user is tired of playing after 10 tries
+// using a "do while" loop
+// this is to avoid the use of recursive functions
 
-if (initialPrompt == "Yes"){
-    return gameFunction();
-} else if(initialPrompt == "No"){
-    console.log("Alright.")
-} else{
-    console.log("Please enter `Yes` or `No`")
-}
-
-function gameFunction(){
-    function getComputerChoice(){
-        var min = 0;
-        var max = 2;
-        function getRandomIntInclusive(min, max) {
-        const minCeiled = Math.ceil(min);
-        const maxFloored = Math.floor(max);
-        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-        };
-
-        // console.log(getRandomIntInclusive(min, max));
-        var computerChoice = getRandomIntInclusive(min, max);
-
-        switch(computerChoice){
-            case 0:
-                return "Rock";
-                break
-        
-            case 1:
-                return "Paper";
-                break
-        
-            case 2:
-                return "Scissors";
-                break
-            default:
-                console.log("Please pick between `Rock, Paper Scissors`.")
-        }
-    }
-    var computerChoiceFinal = getComputerChoice();
-
-    userChoice = prompt("Choose your weapon: Rock || Paper || Scissors-> ");
-    if(userChoice == "Rock" && computerChoiceFinal == "Rock"){
-        console.log("It's a tie!")
-        return gameFunction();
-    }else if(userChoice == "Rock" && computerChoiceFinal == "Paper"){
-        console.log("Better luck next time!");
-        return gameFunction();
-    } else if(userChoice == "Rock" && computerChoiceFinal == "Scissors"){
-        console.log("You knocked me out!");
-        return gameFunction();
-    } else if(userChoice == "Paper" && computerChoiceFinal == "Paper"){
-        console.log("It's a tie!");
-        return gameFunction();
-    } else if(userChoice == "Paper" && computerChoiceFinal == "Rock"){
-        console.log("You win!");
-        return gameFunction();
-    } else if(userChoice == "Paper" && computerChoiceFinal == "Scissors"){
-        console.log("You've been cut up!");
-        return gameFunction();
-    } else if(userChoice == "Scissors" && computerChoiceFinal == "Scissors"){
-        console.log("We're tied up!");
-        return gameFunction();
-    } else if(userChoice == "Scissors" && computerChoiceFinal == "Rock"){
-        console.log("You lose.");
-        return gameFunction();
-    } else if(userChoice == "Scissors" && computerChoiceFinal == "Paper"){
-        console.log("You cut me up!");
-        return gameFunction();
+function gameStart(){
+    const startGame = prompt("Would you like to play Rock, Paper, Scissors? ");
+    if(startGame == "Yes"){
+        return playGame();
     } else{
-        console.log("You didn't choose a weapon, try again.")
-        return gameFunction();
+        console.log("Your wishes are my command.");
     }
 }
+
+function getComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function playGame(){
+    const userChoice = prompt("Choose your weapon: Rock || Paper || Scissors -> ")
+    const computerChoice = getComputerChoice();
+    
+    if(userChoice !== "Rock" && userChoice !== "Paper" && userChoice !== "Scissors"){
+        console.log("You didn't make your play, try again.")
+        return playGame();
+    }
+    
+    console.log(`Computer chose: ${computerChoice}`);
+    console.log(gameOutcomes[userChoice][computerChoice]);
+    return playGame();
+}
+
+const gameOutcomes = {
+  Rock: {
+    Rock: "It's a tie!",
+    Paper: "Better luck next time!",
+    Scissors: "You knocked me out!"
+  },
+  Paper: {
+    Rock: "You win!",
+    Paper: "It's a tie!",
+    Scissors: "You've been cut up!"
+  },
+  Scissors: {
+    Rock: "You lose.",
+    Paper: "You cut me up!",
+    Scissors: "We're tied up!"
+  }
+};
+
+gameStart();
